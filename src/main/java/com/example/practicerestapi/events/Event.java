32cd786810fae99e767a7928f190999f4c72a467
JeongHwa <ekdan31@gmail.com>
2024-09-28 23:11:22 +0900

    FirstCommit

---
+++ src/main/java/com/example/practicerestapi/events/Event.java
@@ -0,0 +1,11 @@
+//package com.example.practicerestapi.events;
+//
+//import java.time.LocalDateTime;
+//
+//public class Event {
+//
+//    private Integer id;
+//    private String name;
+//    private String description;
+//    private LocalDateTime
+//}
